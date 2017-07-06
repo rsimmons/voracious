47998d9a201415d20f954f0be534c6c5a8f0f72a
Russel Simmons <symstym@mac.com>
2017-07-05 20:31:43 -0700

    Fix bug in clearKindInRange

--- util/annotext.js
+++ util/annotext.js
@@ -34,8 +34,8 @@ export const addAnnotation = (annoText, cpBegin, cpEnd, kind, data) => {
   return new AnnotatedText({text: annoText.text, annotations: annoText.annotations.push(new Annotation({cpBegin, cpEnd, kind, data}))});
 };
 
-export const clearKindInRange = (annoText, kind, cpBegin, cpEnd) => {
-  return annoText.update('annotations', annotations => annotations.filter(anno => ((anno.cpEnd <= cpBegin) || (anno.cpBegin >= cpEnd))));
+export const clearKindInRange = (annoText, cpBegin, cpEnd, kind) => {
+  return annoText.update('annotations', annotations => annotations.filter(anno => ((anno.kind !== kind) || (anno.cpEnd <= cpBegin) || (anno.cpBegin >= cpEnd))));
 };
 
 export const getKindAtIndex = (annoText, kind, cpIndex) => {
