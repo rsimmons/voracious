c6eda9dbc1b63eaf9954130589c877515ac23b51
Russel Simmons <symstym@mac.com>
2017-07-08 18:05:29 -0700

    Change how we avoid lint warning

--- src/util/analysis.js
+++ src/util/analysis.js
@@ -1,7 +1,7 @@
 import { hiraToKata, kataToHira, anyCharIsKana } from '../util/japanese';
 import DiffMatchPatch from 'diff-match-patch';
 import { Annotation, create as createAnnoText } from './annotext';
-/*global kuromoji*/ // this is loaded by script tag in index.html
+const kuromoji = window.kuromoji; // loaded by script tag in index.html, we do this to avoid lint warning
 
 const dmp = new DiffMatchPatch();
 
