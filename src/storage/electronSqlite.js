798b3018d1123ac9eb6c5b3cf1d4498973e1a71e
Russel Simmons <symstym@mac.com>
2017-08-30 21:06:42 -0700

    Fix problem with interleaving upserts

--- src/storage/electronSqlite.js
+++ src/storage/electronSqlite.js
@@ -23,9 +23,16 @@ class ElectronSqliteBackend {
 
   async setItem(key, value) {
     // This is supposedly the right way to upsert in sqlite
-    // NOTE: If we had other writers, this would need to be in a transaction
-    await db.run('UPDATE kv SET v = ? WHERE k = ?', value, key);
-    await db.run('INSERT INTO kv (k, v) SELECT ?, ? WHERE changes() = 0', key, value);
+    // NOTE: This wouldn't really be safe if we had multiple calls
+    //  to setItem for same key back to back. There could be a race
+    //  where they both try to INSERT, I think.
+    // TODO: Since all calls go through this same backend object,
+    //  and they're all async, we could serialize them here.
+    const { changes } = await db.run('UPDATE kv SET v = ? WHERE k = ?', value, key);
+    if (changes === 0) {
+      // If changes is 0 that means the UPDATE failed to match any rows
+      await db.run('INSERT INTO kv (k, v) VALUES (?, ?)', key, value);
+    }
   }
 
   async removeItem(key) {
