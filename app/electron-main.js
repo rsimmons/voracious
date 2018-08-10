5cc0dc02642544e45cb99fc9a31d67dca060fc36
Russel Simmons <russ@rsimmons.org>
2018-08-09 18:41:32 -0700

    Fix local URLs with utf-8 filenames

--- app/electron-main.js
+++ app/electron-main.js
@@ -13,7 +13,8 @@ let mainWindow;
 function registerLocalProtocol() {
   protocol.registerFileProtocol('local', (request, callback) => {
     const path = request.url.substr(8); // strip off local:// prefix
-    callback(path)
+    const decodedPath = decodeURI(path); // this makes utf-8 filenames work, I'm not sure it's correct
+    callback(decodedPath);
   }, (error) => {
     if (error) console.error('Failed to register protocol');
   })
