c4e680429b9abb70a2017f6695bb635446786c53
Russel Simmons <russ@rsimmons.org>
2018-08-13 12:32:27 -0700

    Hack temp fix for select alignment

--- src/components/Select.js
+++ src/components/Select.js
@@ -4,7 +4,7 @@ export default class Select extends Component {
   render() {
     const { value, options, onChange } = this.props;
     return (
-      <select value={value} onChange={e => { onChange && onChange(e.target.value)}}>
+      <select style={{verticalAlign: 'bottom'}} value={value} onChange={e => { onChange && onChange(e.target.value)}}>
         {options.map((o, i) => <option key={i} value={o.value} disabled={!!o.disabled}>{o.label}</option>)}
       </select>
     );
