b5f9f715ebd764fb5ac1e1b040872f8a256172d4
Russel Simmons <russ@rsimmons.org>
2018-08-24 11:41:51 -0700

    Add lang codes table, fix sub lang attr

--- src/util/languages.js
+++ src/util/languages.js
@@ -1,6 +1,14 @@
 import franc from 'franc';
 
+import languageTable from './languageTable';
+
+const iso6393ToEntry = new Map(languageTable.map(entry => [entry.iso6393, entry]));
+
 export function detectIso6393(text) {
   // franc returns ISO 639-3 codes, including 'und' for undetermined
   return franc(text);
+}
+
+export function iso6393To6391(iso6393) {
+  return iso6393ToEntry.get(iso6393).iso6391;
 }
