cc1ef6ea1ea9adadff10d37c39cfbb576e5de059
Russel Simmons <russ@rsimmons.org>
2019-02-04 12:42:09 -0800

    Fix copy interception to not conflict, fix #24

--- src/components/CopyInterceptor.js
+++ src/components/CopyInterceptor.js
@@ -2,6 +2,12 @@ import { PureComponent } from 'react';
 
 export default class AnnoText extends PureComponent {
   handleCopy = (e) => {
+    // Do not intercept if there is a proper selection in document
+    const selection = document.getSelection();
+    if (selection && selection.toString()) { // using toString instead of isCollapsed handles selections in inputs
+      return;
+    }
+
     e.preventDefault();
     for (const {format, data} of this.props.copyData) {
       e.clipboardData.setData(format, data);
